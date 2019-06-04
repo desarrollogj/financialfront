79c834b322bb64c0707ad6448dace5af8ad6cd99
desarrollogj <desarrolloglj@gmail.com>
2019-06-04 19:48:39 -0300

    Added polling option to GetCurrency method

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../rest.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { interval } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-currency',
@@ -8,18 +10,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
-
-  currencies:any = [];
+  currencies: any = [];
+  pollingInterval = 300000; // 5 minutes
 
   constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.getCurrencies();
+    this.getPolledCurrencies();
   }
 
   getCurrencies() {
-    this.currencies = [];
     this.rest.getCurrencies().subscribe((data: {}) => {
+      this.currencies = data;
+    });
+  }
+
+  getPolledCurrencies() {
+    interval(this.pollingInterval).pipe(
+      switchMap(() => this.rest.getCurrencies().pipe(
+        map(res => res)
+      )),
+    ).subscribe(data => {
       this.currencies = data;
     });
   }
